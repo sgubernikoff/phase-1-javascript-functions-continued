f27bc621c480a8085b6d7aa71962fb32f001e5da
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-02-02 12:30:04 -0500

    commit tree b62ad8cc7a1e4094f1e9154a17abfd0667176ce7

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 // code your solution here
-const saturdayFun = function (a = "roller-skate") {
-  return `This Saturday, I want to ${a}!`;
-};
-saturdayFun("bathe my dog");
+// const saturdayFun = function (a = "roller-skate") {
+//   return `This Saturday, I want to ${a}!`;
+// };
+// saturdayFun("bathe my dog");
 
-const mondayWork = function (b = "go to the office") {
-  return `This Monday, I will ${b}.`;
-};
+// const mondayWork = function (b = "go to the office") {
+//   return `This Monday, I will ${b}.`;
+// };
 
-const wrapAdjective = function (flair = "*") {
-  return function (adj = "special") {
-    return `You are ${flair}${adj}${flair}!`;
+// const wrapAdjective = function (flair = "*") {
+//   return function (adj = "special") {
+//     return `You are ${flair}${adj}${flair}!`;
+//   };
+// };
+
+function saturdayFun(go = "roller-skate") {
+  return `This Saturday, I want to ${go}!`;
+}
+
+function mondayWork(no = "go to the office") {
+  return `This Monday, I will ${no}.`;
+}
+
+function wrapAdjective(so = "*") {
+  return function sayIt(ring = "special") {
+    return `You are ${so}${ring}${so}!`;
   };
-};
+}
